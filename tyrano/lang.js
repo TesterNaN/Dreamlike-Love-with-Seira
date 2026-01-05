803eed4e99316d8ec2cff8c1c2292b9dfd8f2f42
TesterNaN <2571597949@qq.com>
2026-01-05 14:42:29 +0800

    game upload

---
+++ tyrano/lang.js
@@ -0,0 +1,29 @@
+window.tyrano_lang = {
+  word: {
+    "go_title":"即将回到标题画面，您确定吗？",
+    "exit_game":"即将关闭窗口并结束游戏，您确定吗？",
+    "not_saved":"没有数据",
+    "tag":"标识",
+    "not_exists":"不存在",
+    "error":"发生错误，请确认脚本。",
+    "label":"标签",
+    "label_double":"在同一剧本文件中有重复。",
+    "error_occurred":"发生错误。",
+    "save_file_violation_1":"已检测对存档的非常规操作，停止启动游戏。",
+    "save_file_violation_2":"停止启动游戏。请删除存档并再次尝试启动游戏。"
+  },
+
+  novel: {
+    "file_menu_button_save":"menu_button_save.gif",
+    "file_menu_button_load":"menu_button_load.gif",
+    "file_menu_button_message_close":"menu_message_close.gif",
+    "file_menu_button_skip":"menu_button_skip.gif",
+    "file_menu_button_title":"menu_button_title.gif",
+    "file_menu_button_close":"menu_button_close.png",
+    "file_menu_bg":"menu_bg.jpg",
+    "file_save_bg":"menu_save_bg.jpg",
+    "file_load_bg":"menu_load_bg.jpg",
+    "file_button_menu":"button_menu.png",
+    "error_occurred":"error occurred"
+  }
+};
